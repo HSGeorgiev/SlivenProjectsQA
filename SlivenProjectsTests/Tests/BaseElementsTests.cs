b30141e5dd5c01c2686f735380f01b5526f9795a
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-05 14:04:18 +0300

    Page, accessible from 'By status' menu added to TopMenu_LinksTexts_ShouldBeProper test.

--- SlivenProjectsTests/Tests/BaseElementsTests.cs
+++ SlivenProjectsTests/Tests/BaseElementsTests.cs
@@ -9,8 +9,8 @@ namespace SlivenProjectsTests.Tests
 {
     internal class BaseElementsTests : BaseTests
     {
-        
-        //Pages, accessible from the top menu
+
+        //TopMenu_LinksTexts_ShouldBeProper - Pages, accessible from the top menu
         [TestCase("Home","/")]
         [TestCase("Начало", "/index.php?option=com_sobi2&Itemid=87")]
         [TestCase("Структурни фондове на ЕС", "/index.php?option=com_sobi2&catid=9&Itemid=53")]
@@ -19,13 +19,20 @@ namespace SlivenProjectsTests.Tests
         [TestCase("Национални програми", "/index.php?option=com_sobi2&catid=6&Itemid=53")]
         [TestCase("Търсене", "/index.php?option=com_sobi2&sobi2Task=search&Itemid=53")]
 
-        //Pages, accessible from the 'InTheRegister' menu
+        //TopMenu_LinksTexts_ShouldBeProper - Pages, accessible from the 'InTheRegister' menu
         [TestCase("Проекти", "/index.php?option=com_sobi2&Itemid=86")]
         [TestCase("Търсене в регистъра", "/index.php?option=com_sobi2&sobi2Task=search&Itemid=53")]
         [TestCase("Структурни фондове на Европейския съюз", "/index.php?option=com_sobi2&catid=9&Itemid=53")]
         [TestCase("Други инструменти на Европейския съюз", "/index.php?option=com_sobi2&catid=8&Itemid=53")]
         [TestCase("Предприсъединителни инструменти", "/index.php?option=com_sobi2&catid=7&Itemid=53")]
         [TestCase("Други наднационални инструменти", "/index.php?option=com_sobi2&catid=59&Itemid=53")]
+
+        //TopMenu_LinksTexts_ShouldBeProper Pages, accessible from the 'By status' menu
+        [TestCase("Приключили проекти", "/index.php?option=com_sobi2&catid=33&Itemid=53")]
+        [TestCase("Текущи проекти", "/index.php?option=com_sobi2&catid=34&Itemid=53")]
+        [TestCase("Очакващи одобрение и финансиране", "/index.php?option=com_sobi2&catid=35&Itemid=53")]
+
+
 
         public void TopMenu_LinksTexts_ShouldBeProper(string pageHeading, string subUrl)
         {
