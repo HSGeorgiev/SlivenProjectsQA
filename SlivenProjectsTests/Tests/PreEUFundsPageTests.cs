a34203904e37c5afbd78fcb056842a16fc5c1eca
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-19 19:42:07 +0300

    PreEUFundsTests class added with tests.

---
+++ SlivenProjectsTests/Tests/PreEUFundsPageTests.cs
@@ -0,0 +1,44 @@
+﻿using SlivenProjectsTests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlivenProjectsTests.Tests
+{
+    internal class PreEUFundsPageTests  : BaseTests
+    {
+
+        string subUrl = "/index.php?option=com_sobi2&catid=9&Itemid=53";
+
+        [Test]
+        public void TopMenu_LinksTexts_ShouldBeProper()
+        {
+            var preEUFundsPage = new PreEUFundsPage(driver);
+            preEUFundsPage.GoToTargetPage(subUrl);
+            bool[] topMenuChecks = preEUFundsPage.menuLinksTextsCheck(preEUFundsPage.topMenuItems, preEUFundsPage.topMenuTexts);
+
+            for (int i = 0; i < topMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(topMenuChecks[i], $"Top menu item {preEUFundsPage.topMenuTexts[i]} " +
+                    $"should be {preEUFundsPage.topMenuTexts[i]}, but is not");
+            }
+        }
+
+        [Test]
+        public void InRegisterMenu_LinksTexts_ShouldBeProper()
+        {
+            var preEUFundsPage = new PreEUFundsPage(driver);
+            preEUFundsPage.GoToTargetPage(subUrl);
+            bool[] topMenuChecks = preEUFundsPage.menuLinksTextsCheck(preEUFundsPage.inRegisterMenuItems, preEUFundsPage.inRegisterMenuTexts);
+
+            for (int i = 0; i < topMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(topMenuChecks[i], $"InRegister menu item {preEUFundsPage.inRegisterMenuTexts[i]} " +
+                    $"should be {preEUFundsPage.inRegisterMenuTexts[i]}, but is not");
+            }
+        }
+
+    }
+}
