a9958ad94c6d719ef084f7bc3346172a598e0323
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-19 19:38:36 +0300

    PreEUFundsPaage class added.

---
+++ SlivenProjectsTests/Pages/PreEUFundsPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlivenProjectsTests.Pages
+{
+    internal class PreEUFundsPage : BasePage
+    {
+        // Descript the element and logic on the Pre EU Funds page,
+        // excluding base elements, common for all pages.
+        public PreEUFundsPage(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+}
