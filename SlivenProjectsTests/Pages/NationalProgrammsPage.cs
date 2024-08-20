b6bfc4a8f5375e834302352a6ef67d4ae1e1bea0
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-20 18:08:04 +0300

    NationalProgrammsPage class added.

---
+++ SlivenProjectsTests/Pages/NationalProgrammsPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlivenProjectsTests.Pages
+{
+    internal class NationalProgrammsPage : BasePage
+    {
+        public NationalProgrammsPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public new string pageUrl => BASE_URL + "/index.php?option=com_sobi2&catid=6&Itemid=53";
+    }
+}
