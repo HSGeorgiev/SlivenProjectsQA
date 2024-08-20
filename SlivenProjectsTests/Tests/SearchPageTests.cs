e6ad01cb23a06c55d324f45e6674ccec3473635a
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-20 18:34:57 +0300

    InRegisterMenu_LinksTexts_ShouldBeProper() test added to  : BaseTests class

--- SlivenProjectsTests/Tests/SearchPageTests.cs
+++ SlivenProjectsTests/Tests/SearchPageTests.cs
@@ -23,6 +23,21 @@ namespace SlivenProjectsTests.Tests
                     $"should be {searchPage.topMenuTexts[i]}, but is not");
             }
         }
+
+
+        [Test]
+        public void InRegisterMenu_LinksTexts_ShouldBeProper()
+        {
+            var searchPage = new SearchPage(driver);
+            searchPage.GoToTargetPage(searchPage.pageUrl);
+            bool[] topMenuChecks = searchPage.menuLinksTextsCheck(searchPage.inRegisterMenuItems, searchPage.inRegisterMenuTexts);
+
+            for (int i = 0; i < topMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(topMenuChecks[i], $"InRegister menu item {searchPage.inRegisterMenuTexts[i]} " +
+                    $"should be {searchPage.inRegisterMenuTexts[i]}, but is not");
+            }
+        }
     }
 
 
