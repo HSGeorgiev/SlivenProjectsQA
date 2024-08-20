46ad5c677e3ac77702d2742979dfbe8402080171
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-20 18:28:47 +0300

    SearchPage class added.

---
+++ SlivenProjectsTests/Pages/SearchPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlivenProjectsTests.Pages
+{
+    internal class SearchPage : BasePage
+    {
+        public SearchPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public By registerLink => By.XPath("//td[@id='sobi2HeaderComLink']/a");
+        public By searchLink => By.XPath("//td[@id='sobi2HeaderComLink']/a");
+
+
+    }
+}
