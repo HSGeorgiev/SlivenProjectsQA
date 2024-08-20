a5ba116cb12c3adb656fc50839852e44b136e7e9
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-20 18:43:16 +0300

    Main elements on search page without search form added as By

--- SlivenProjectsTests/Pages/SearchPage.cs
+++ SlivenProjectsTests/Pages/SearchPage.cs
@@ -14,8 +14,15 @@ namespace SlivenProjectsTests.Pages
         }
 
         public new string pageUrl => BASE_URL + "/index.php?option=com_sobi2&sobi2Task=search&Itemid=53";
-
-
+        public By registerLinkElement => By.XPath("//td[@id='sobi2HeaderComLink']/a");
+        public By searchLinkElement => By.XPath("//td[@id='sobi2HeaderComLink']/a");
+        public By searchInputElement => By.XPath("//input[@id='sobi2Search']");
+        public By searchButtonElement => By.XPath("//input[@id='sobiSearchSubmitBt']");
+        public By anyWordRadioButtonElement => By.XPath("//input[@id='searchphraseany']");
+        public By allWordRadioButtonElement => By.XPath("//input[@id='searchphraseall']");
+        public By exactPhraseRadioButtonElement => By.XPath("//input[@id='searchphraseexact']");
+        public By searchFormSlideButtonElement => By.XPath("//input[@id='sobiSearchFormExtOptToggle']");
+        public By clearFormButtonElement => By.XPath("sobiSearchFormExtOptToggle");
 
     }
 }
