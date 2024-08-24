d7109066e9032859ef9e8fe039a4990476810d10
HSGeorgiev <hsgeorgiev@gmail.com>
2024-08-24 21:22:46 +0300

    A bit more descriptive

--- SlivenProjectsTests/Globals/GlobalConstants.cs
+++ SlivenProjectsTests/Globals/GlobalConstants.cs
@@ -8,8 +8,12 @@ namespace SlivenProjectsTests.Globals
 {
     public class GlobalConstants
     {
+        //The base URL of the target site/app
         public const string BASE_URL = "https://projects.sliven.bg/";
+        //The default timeout for the explicit waits in seconds
         public const int DEFAULT_TIMEOUT = 10;
+        //The title of the project for the Exceptions' message
+        //handlings description messages
         public const string PR_TITLE = "SLIVEN_PROJECTS_TESTS";
     }
 }
