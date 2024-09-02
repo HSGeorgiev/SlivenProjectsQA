9a369a292a019797923f2252bc0ad7c77b12475f
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-02 15:53:18 +0300

    public void RoleOfSliven_LinksTexts_ShouldBeProper() test added to NationalProgrammsPageTests class

--- SlivenProjectsTests/Tests/NationalProgrammsPageTests.cs
+++ SlivenProjectsTests/Tests/NationalProgrammsPageTests.cs
@@ -33,7 +33,7 @@ namespace SlivenProjectsTests.Tests
         }
 
         [Test]
-        public void byStatus_LinksTexts_ShouldBeProper()
+        public void ByStatus_LinksTexts_ShouldBeProper()
         {
             var nationalProgrammsPage = new NationalProgrammsPage(driver);
             nationalProgrammsPage.GoToTargetPage(nationalProgrammsPage.pageUrl);
@@ -45,6 +45,20 @@ namespace SlivenProjectsTests.Tests
                 Console.WriteLine(nationalProgrammsPage.byStatusMenuTexts[i]);
 
                 Assert.IsTrue(byStatusMenuChecks[i], $"ByProjects Status menu item {nationalProgrammsPage.byStatusMenuTexts[i]} " +
+                    $"should be {nationalProgrammsPage.byStatusMenuTexts[i]}, but is not");
+            }
+        }
+
+        [Test]
+        public void RoleOfSliven_LinksTexts_ShouldBeProper()
+        {
+            var nationalProgrammsPage = new NationalProgrammsPage(driver);
+            nationalProgrammsPage.GoToTargetPage(nationalProgrammsPage.pageUrl);
+            bool[] roleMenuChecks = nationalProgrammsPage.menuLinksTextsCheck(nationalProgrammsPage.roleOfSlivenMunMenuItems, nationalProgrammsPage.roleOfSlivenMunMenuTexts);
+
+            for (int i = 0; i < roleMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(roleMenuChecks[i], $"By Role Of Sliven menu item {nationalProgrammsPage.roleOfSlivenMunMenuTexts[i]} " +
                     $"should be {nationalProgrammsPage.byStatusMenuTexts[i]}, but is not");
             }
         }
