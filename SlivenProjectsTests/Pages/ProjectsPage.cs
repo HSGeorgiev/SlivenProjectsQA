bde8e6a1bc15c3d2b289c7845bc8a37257fd3c54
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-05 14:22:33 +0300

    Base properties of ProjectsPage class

--- SlivenProjectsTests/Pages/ProjectsPage.cs
+++ SlivenProjectsTests/Pages/ProjectsPage.cs
@@ -4,10 +4,14 @@ namespace SlivenProjectsTests.Pages
 {
     internal class ProjectsPage : BasePage
     {
+        // Descript the element and logic on the Projects page,
+        // excluding base elements, common for all pages.
         public ProjectsPage(IWebDriver driver) : base(driver)
         {
         }
 
         public new string pageUrl => BASE_URL + "/index.php?option=com_sobi2&Itemid=86";
+
+        public By categoriesNames => By.XPath("//p[@class='sobi2CatName']");
     }
 }
