2f04803de02651efebf00c5651082c28aeb6e1c9
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-05 14:36:18 +0300

    TopMenu_LinksTexts_ShouldBeProper() test added to ProjectsPageTests class

--- SlivenProjectsTests/Tests/ProjectsPageTests.cs
+++ SlivenProjectsTests/Tests/ProjectsPageTests.cs
@@ -10,6 +10,18 @@ namespace SlivenProjectsTests.Tests
     internal class ProjectsPageTests : BaseTests
     {
 
-     
+        [Test]
+        public void TopMenu_LinksTexts_ShouldBeProper()
+        {
+            var projectsPage = new ProjectsPage(driver);
+            projectsPage.GoToTargetPage(projectsPage.pageUrl);
+            bool[] topMenuChecks = projectsPage.menuLinksTextsCheck(projectsPage.topMenuItems, projectsPage.topMenuTexts);
+
+            for (int i = 0; i < topMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(topMenuChecks[i], $"Top menu item {projectsPage.topMenuTexts[i]} " +
+                    $"should be {projectsPage.topMenuTexts[i]}, but is not");
+            }
+        }
     }
 }
