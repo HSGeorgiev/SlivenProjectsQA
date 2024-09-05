6ba6b8cfc4cb9f008d53e954f68f6bff495282a4
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-05 14:14:18 +0300

    ProjectsPage added

---
+++ SlivenProjectsTests/Pages/ProjectsPage.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace SlivenProjectsTests.Pages
+{
+    internal class ProjectsPage : BasePage
+    {
+        public ProjectsPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public new string pageUrl => BASE_URL + "/index.php?option=com_sobi2&Itemid=86";
+    }
+}
