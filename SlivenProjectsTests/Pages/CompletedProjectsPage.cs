f4bb21194d8774d0c607d65b8e52798de5fc7c20
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-12 16:47:25 +0300

    CompletedProjectsPage class added to represent Complited Projects Page

---
+++ SlivenProjectsTests/Pages/CompletedProjectsPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlivenProjectsTests.Pages
+{
+    internal class CompletedProjectsPage : BasePage
+    {
+        public CompletedProjectsPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public new string pageUrl => BASE_URL + "/index.php?option=com_sobi2&catid=33&Itemid=53";
+    }
+}
