cacdbb5ea599c457abe8d8a41fab6b8b8d96d952
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-12 16:53:41 +0300

    TopMenu_LinksTexts_ShouldBeProper test added to CompletedProjectsPageTests class

--- SlivenProjectsTests/Tests/CompletedProjectsPageTests.cs
+++ SlivenProjectsTests/Tests/CompletedProjectsPageTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SlivenProjectsTests.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,19 @@ namespace SlivenProjectsTests.Tests
 {
     internal class CompletedProjectsPageTests : BaseTests
     {
+
+        [Test]
+        public void TopMenu_LinksTexts_ShouldBeProper()
+        {
+            var completedProjectsPage = new CompletedProjectsPage(driver);
+            completedProjectsPage.GoToTargetPage(completedProjectsPage.pageUrl);
+            bool[] topMenuChecks = completedProjectsPage.menuLinksTextsCheck(completedProjectsPage.topMenuItems, completedProjectsPage.topMenuTexts);
+
+            for (int i = 0; i < topMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(topMenuChecks[i], $"Top menu item {completedProjectsPage.topMenuTexts[i]} " +
+                    $"should be {completedProjectsPage.topMenuTexts[i]}, but is not");
+            }
+        }
     }
 }
