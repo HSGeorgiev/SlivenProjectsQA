23ed334d25230861fad086a84f96ad3a881b286d
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-14 16:45:03 +0300

    TopMenu_LinksTexts_ShouldBeProper test added to AwaitingProjectsPageTests class

--- SlivenProjectsTests/Tests/AwaitingProjectsPageTests.cs
+++ SlivenProjectsTests/Tests/AwaitingProjectsPageTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SlivenProjectsTests.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace SlivenProjectsTests.Tests
 {
     internal class AwaitingProjectsPageTests : BaseTests
     {
+        [Test]
+        public void TopMenu_LinksTexts_ShouldBeProper()
+        {
+            AwaitingProjectsPage awaitingProjectsPage = new AwaitingProjectsPage (driver);
+            awaitingProjectsPage.GoToTargetPage(awaitingProjectsPage.pageUrl);
+            bool[] topMenuChecks = awaitingProjectsPage.menuLinksTextsCheck(awaitingProjectsPage.topMenuItems, awaitingProjectsPage.topMenuTexts);
+
+            for (int i = 0; i < topMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(topMenuChecks[i], $"Top menu item {awaitingProjectsPage.topMenuTexts[i]} " +
+                    $"should be {awaitingProjectsPage.topMenuTexts[i]}, but is not");
+            }
+        }
+
     }
+
+
 }
