c2707b9ea9bb15f21d3b640b1054d4e763e58186
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-14 16:38:24 +0300

    AwaitingProjectsPage class created to represent 'Projects awaiting funding' Page

---
+++ SlivenProjectsTests/Pages/AwaitingProjectsPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlivenProjectsTests.Pages
+{
+    internal class AwaitingProjectsPage : BasePage
+    {
+        public AwaitingProjectsPage(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+}
