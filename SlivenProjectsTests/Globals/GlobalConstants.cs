9ab01b25c8fbeba244f3978b428fda75654438d9
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-15 19:56:26 +0300

    Cleaning up

--- SlivenProjectsTests/Globals/GlobalConstants.cs
+++ SlivenProjectsTests/Globals/GlobalConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SlivenProjectsTests.Globals
+﻿namespace SlivenProjectsTests.Globals
 {
     public class GlobalConstants
     {
