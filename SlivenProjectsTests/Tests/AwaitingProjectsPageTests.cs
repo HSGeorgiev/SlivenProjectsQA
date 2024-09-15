f64d8bfdc32fc5cf6540db49017f9067f0ad0670
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-15 19:46:08 +0300

    ByStatus_LinksTexts_ShouldBeProper test added to AwaitingProjectsPageTests class

--- SlivenProjectsTests/Tests/AwaitingProjectsPageTests.cs
+++ SlivenProjectsTests/Tests/AwaitingProjectsPageTests.cs
@@ -32,6 +32,23 @@ namespace SlivenProjectsTests.Tests
             }
         }
 
+        [Test]
+        public void ByStatus_LinksTexts_ShouldBeProper()
+        {
+            AwaitingProjectsPage awaitingProjectsPage = new AwaitingProjectsPage(driver);
+            awaitingProjectsPage.GoToTargetPage(awaitingProjectsPage.pageUrl);
+            bool[] byStatusMenuChecks = awaitingProjectsPage.menuLinksTextsCheck(awaitingProjectsPage.byStatusMenuItems, awaitingProjectsPage.byStatusMenuTexts);
+
+            for (int i = 0; i < byStatusMenuChecks.Length; i++)
+            {
+                Console.WriteLine(byStatusMenuChecks[i]);
+                Console.WriteLine(awaitingProjectsPage.byStatusMenuTexts[i]);
+
+                Assert.IsTrue(byStatusMenuChecks[i], $"ByProjects Status menu item {awaitingProjectsPage.byStatusMenuTexts[i]} " +
+                    $"should be {awaitingProjectsPage.byStatusMenuTexts[i]}, but is not");
+            }
+        }
+
     }
 
 
