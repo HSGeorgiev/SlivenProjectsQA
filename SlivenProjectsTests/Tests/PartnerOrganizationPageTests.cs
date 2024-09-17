483aefaa41fec75f64b9fec7f296bb161cc50261
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-17 18:03:08 +0300

    Fix

--- SlivenProjectsTests/Tests/PartnerOrganizationPageTests.cs
+++ SlivenProjectsTests/Tests/PartnerOrganizationPageTests.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SlivenProjectsTests.Tests
 {
-    internal class PPartnerOrganizationPageTests : BaseTests
+    internal class PartnerOrganizationPageTests : BaseTests
     {
     }
 }
