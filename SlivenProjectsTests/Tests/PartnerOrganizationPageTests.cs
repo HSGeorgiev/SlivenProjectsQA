3e965f75199645c4963021dc4453fd09ba205c53
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-17 18:05:33 +0300

    TopMenu_LinksTexts_ShouldBeProper test added to PartnerOrganizationPageTests class

--- SlivenProjectsTests/Tests/PartnerOrganizationPageTests.cs
+++ SlivenProjectsTests/Tests/PartnerOrganizationPageTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SlivenProjectsTests.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace SlivenProjectsTests.Tests
 {
     internal class PartnerOrganizationPageTests : BaseTests
     {
+        [Test]
+        public void TopMenu_LinksTexts_ShouldBeProper()
+        {
+            var partnerOrganizationPage = new PartnerOrganizationPage(driver);
+            partnerOrganizationPage.GoToTargetPage(partnerOrganizationPage.pageUrl);
+            bool[] topMenuChecks = partnerOrganizationPage.menuLinksTextsCheck(partnerOrganizationPage.topMenuItems, partnerOrganizationPage.topMenuTexts);
+
+            for (int i = 0; i < topMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(topMenuChecks[i], $"Top menu item {partnerOrganizationPage.topMenuTexts[i]} " +
+                    $"should be {partnerOrganizationPage.topMenuTexts[i]}, but is not");
+            }
+        }
     }
 }
