500ca2d6f08f69688c474c4487ca74b69c2003b8
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-19 16:13:21 +0300

    CurrentYearPageTests class added to contains test for CurrentYearPage tests

--- SlivenProjectsTests/Pages/CurrentYearPage.cs
+++ SlivenProjectsTests/Pages/CurrentYearPage.cs
@@ -12,5 +12,7 @@ namespace SlivenProjectsTests.Pages
         public CurrentYearPage(IWebDriver driver) : base(driver)
         {
         }
+
+        public new string pageUrl => BASE_URL + "/index.php?option=com_sobi2&catid=87&Itemid=53";
     }
 }
---
+++ SlivenProjectsTests/Tests/CurrentYearPageTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlivenProjectsTests.Tests
+{
+    internal class CurrentYearPageTests : BaseTests
+    {
+
+
+    }
+}
