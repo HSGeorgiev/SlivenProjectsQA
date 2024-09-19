beee4eb27446330f19e332b3d33627a6713a4167
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-19 16:16:22 +0300

    TopMenu_LinksTexts_ShouldBeProper test added to CurrentYearPage class

--- SlivenProjectsTests/Tests/CurrentYearPageTests.cs
+++ SlivenProjectsTests/Tests/CurrentYearPageTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SlivenProjectsTests.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,19 @@ namespace SlivenProjectsTests.Tests
 {
     internal class CurrentYearPageTests : BaseTests
     {
+        [Test]
+        public void TopMenu_LinksTexts_ShouldBeProper()
+        {
+            var currentYearPage = new CurrentYearPage(driver);
+            currentYearPage.GoToTargetPage(currentYearPage.pageUrl);
+            bool[] topMenuChecks = currentYearPage.menuLinksTextsCheck(currentYearPage.topMenuItems, currentYearPage.topMenuTexts);
 
+            for (int i = 0; i < topMenuChecks.Length; i++)
+            {
+                Assert.IsTrue(topMenuChecks[i], $"Top menu item {currentYearPage.topMenuTexts[i]} " +
+                    $"should be {currentYearPage.topMenuTexts[i]}, but is not");
+            }
+        }
 
     }
 }
