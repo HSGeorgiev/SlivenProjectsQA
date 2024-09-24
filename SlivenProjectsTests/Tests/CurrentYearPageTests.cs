6abafcc896deab0ea14ff1c16e45b7c9163c1e67
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-24 19:03:30 +0300

    HeadingTextShouldBeCorect test added to CurrentYearPageTests class

--- SlivenProjectsTests/Tests/CurrentYearPageTests.cs
+++ SlivenProjectsTests/Tests/CurrentYearPageTests.cs
@@ -18,7 +18,18 @@ namespace SlivenProjectsTests.Tests
             Assert.IsTrue(footerTextActual == footerTextExpected, "Footer text should be correct");
         }
 
+        [Test]
+        public void HeadingTextShouldBeCorect()
+        {
+            var currentYearPage = new CurrentYearPage(driver);
+            currentYearPage.GoToTargetPage(BASE_URL);
 
+            string headingTextActual = currentYearPage.GetText(currentYearPage.pageHeading);
+            string headingTextExpected = "Регистър за проекти - Община Сливен";
+            //Console.WriteLine(headingTextActual);
+            //Console.WriteLine(headingTextExpected);
+            Assert.IsTrue(headingTextActual == headingTextExpected, "Footer text should be correct");
+        }
 
         [Test]
         public void TopMenu_LinksTexts_ShouldBeProper()
