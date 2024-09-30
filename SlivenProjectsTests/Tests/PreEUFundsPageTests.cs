1a1c70d7ce44907cf1020a19ddf4d9b8bde4e76a
HSGeorgiev <hsgeorgiev@gmail.com>
2024-09-30 18:53:40 +0300

    HeadingTextShouldBeCorect test added to PreEUFundsPageTests class

--- SlivenProjectsTests/Tests/PreEUFundsPageTests.cs
+++ SlivenProjectsTests/Tests/PreEUFundsPageTests.cs
@@ -18,7 +18,18 @@ namespace SlivenProjectsTests.Tests
             Assert.IsTrue(footerTextActual == footerTextExpected, "Footer text should be correct");
         }
 
+        [Test]
+        public void HeadingTextShouldBeCorect()
+        {
+            PreEUFundsPage preEUFundsPage = new PreEUFundsPage(driver);
+            preEUFundsPage.GoToTargetPage(BASE_URL);
 
+            string headingTextActual = preEUFundsPage.GetText(preEUFundsPage.pageHeading);
+            string headingTextExpected = "Регистър за проекти - Община Сливен";
+            //Console.WriteLine(headingTextActual);
+            //Console.WriteLine(headingTextExpected);
+            Assert.IsTrue(headingTextActual == headingTextExpected, "Footer text should be correct");
+        }
 
 
 
